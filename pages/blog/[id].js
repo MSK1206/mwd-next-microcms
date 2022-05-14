1ab652dc4845aa0f512aeacba92225a3fafeaa66
MSK1206 <phantom7216@yahoo.co.jp>
2022-05-14 10:16:33 +0900

    0514 git add .

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,11 +1,17 @@
 import { client } from "../../libs/client"
 import styles from '../../styles/Custom.module.scss'
 
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 export default function BlogId({ blog }) {
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>{blog.title}</h1>
-      <p className={styles.publishedAt}>{blog.publishedAt}</p>
+      <p className={styles.publishedAt}>投稿日：{dayjs.utc(blog.publishedAt).tz('Asia/Tokyo').format('YYYY'+ '年' + 'MM' + '月' + 'DD' + '日' + 'hh' + ':' + 'mm')}</p>
       <div
         dangerouslySetInnerHTML={{
           __html: `${blog.content}`,

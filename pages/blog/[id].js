b7521589436f43a3deead98279770b5e5b16881c
MSK1206 <phantom7216@yahoo.co.jp>
2022-05-15 15:03:05 +0900

    git commit 0515 ios vewport fix it

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -13,7 +13,7 @@ export default function BlogId({ blog }) {
     <div className={styles.container}>
       <Head>
       <title>{blog.title} - Matsushita Web Design</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width, viewport-fit=cover" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
       <meta name="description" content="大阪のフリーランスWEBデザイン事務所" />
       <link rel="icon" href="/favicon.svg" />
       </Head>

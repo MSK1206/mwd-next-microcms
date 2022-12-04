f33ab6ec9162ffe1857df3cd1c5e82804b1e3319
Masaki Matsushita <86665622+MSK1206@users.noreply.github.com>
2022-12-04 12:01:03 +0900

    update contact

--- pages/contact.js
+++ pages/contact.js
@@ -5,13 +5,13 @@ import { useEffect } from 'react'
 import styles from '../styles/Some.module.css'
 
 export default function Contact() {
-    useEffect(() => {
-    const head = document.getElementsByTagName("head")[0] as HTMLElement;
-    const scriptUrl = document.createElement("script");
-    scriptUrl.src = "https://sdk.form.run/js/v2/embed.js";
-    head.appendChild(scriptUrl);
-    return () => {
-      head.removeChild(scriptUrl);
+  useEffect(() => {
+  const head = document.getElementsByTagName("head")[0] as HTMLElement;
+  const scriptUrl = document.createElement("script");
+  scriptUrl.src = "https://sdk.form.run/js/v2/embed.js";
+  head.appendChild(scriptUrl);
+  return () => {
+    head.removeChild(scriptUrl);
     };
   }, []);
   return (
